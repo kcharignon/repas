54e21ee862bfbcd21a90ca2a42a6c4f84716913e
Kantin Charignon <kantincharignon@gmail.com>
2025-01-14 16:13:48 +0100

    feat: monter de version en php 8.4.2

--- migrations/Version20250114151250.php
+++ migrations/Version20250114151250.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250114143543 extends AbstractMigration
+final class Version20250114151250 extends AbstractMigration
 {
     public function getDescription(): string
     {
