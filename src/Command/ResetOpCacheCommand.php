47de640cc907bb6dd606e85c7e7f43ef7bb33988
Kantin Charignon <kantincharignon@gmail.com>
2025-02-08 21:32:48 +0100

    fix: reset-opcache command

--- src/Command/ResetOpCacheCommand.php
+++ src/Command/ResetOpCacheCommand.php
@@ -11,6 +11,12 @@ class ResetOpCacheCommand extends Command
     protected static $defaultName = 'repas:reset-opcache';
     protected static $defaultDescription = 'Réinitialise l\'OPcache PHP';
 
+    protected function configure(): void
+    {
+        $this->setName(self::$defaultName)
+            ->setDescription(self::$defaultDescription);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         if (!function_exists('opcache_reset')) {
