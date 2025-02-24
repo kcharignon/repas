9e4dbfac5e57c81ffb36c3c344896c0c5f2ce76d
Kantin Charignon <kantincharignon@gmail.com>
2025-02-24 10:30:16 +0100

    feat: add coverage to phpunit command + add test to RemoveIngredientToShoppingListHandlerTest

--- tests/Repas/Application/RemoveIngredientToShoppingListHandlerTest.php
+++ tests/Repas/Application/RemoveIngredientToShoppingListHandlerTest.php
@@ -6,8 +6,10 @@ namespace Repas\Application;
 use PHPUnit\Framework\TestCase;
 use Repas\Repas\Application\RemoveIngredientToShoppingList\RemoveIngredientToShoppingListCommand;
 use Repas\Repas\Application\RemoveIngredientToShoppingList\RemoveIngredientToShoppingListHandler;
+use Repas\Repas\Domain\Exception\ShoppingListException;
 use Repas\Repas\Domain\Interface\IngredientRepository;
 use Repas\Repas\Domain\Interface\ShoppingListRepository;
+use Repas\Repas\Domain\Model\Ingredient;
 use Repas\Repas\Domain\Model\ShoppingListIngredient;
 use Repas\Repas\Domain\Model\ShoppingListRow;
 use Repas\Tests\Helper\Builder\IngredientBuilder;
@@ -17,6 +19,7 @@ use Repas\Tests\Helper\InMemoryRepository\IngredientInMemoryRepository;
 use Repas\Tests\Helper\InMemoryRepository\ShoppingListInMemoryRepository;
 use Repas\Tests\Helper\InMemoryRepository\UserInMemoryRepository;
 use Repas\User\Domain\Interface\UserRepository;
+use Repas\User\Domain\Model\User;
 
 class RemoveIngredientToShoppingListHandlerTest extends TestCase
 {
@@ -24,6 +27,9 @@ class RemoveIngredientToShoppingListHandlerTest extends TestCase
     private UserRepository $userRepository;
     private ShoppingListRepository $shoppingListRepository;
     private IngredientRepository $ingredientRepository;
+
+    private User $user;
+    private Ingredient $ingredient;
 
     protected function setUp(): void
     {
@@ -35,24 +41,25 @@ class RemoveIngredientToShoppingListHandlerTest extends TestCase
             $this->shoppingListRepository,
             $this->ingredientRepository,
         );
+
+        $this->user = new UserBuilder()->withId('user-id')->build();
+        $this->userRepository->save($this->user);
+        $this->ingredient = new IngredientBuilder()->isPasta()->build();
+        $this->ingredientRepository->save($this->ingredient);
     }
 
     public function testHandleSuccessfullyRemoveIngredientToShoppingListHandlerRemoveIngredient(): void
     {
         // Arrange
-        $user = new UserBuilder()->build();
-        $this->userRepository->save($user);
-        $ingredient = new IngredientBuilder()->isPasta()->build();
-        $this->ingredientRepository->save($ingredient);
         $shoppingList = new ShoppingListBuilder()
-            ->withOwner($user)
-            ->addIngredient($ingredient)
+            ->withId('shopping-list-id')
+            ->withOwner($this->user)
+            ->addIngredient($this->ingredient)
             ->build();
         $this->shoppingListRepository->save($shoppingList);
-
         $command = new RemoveIngredientToShoppingListCommand(
-            $user->getId(),
-            $ingredient->getSlug(),
+            'user-id',
+            'pate',
         );
 
 
@@ -60,7 +67,7 @@ class RemoveIngredientToShoppingListHandlerTest extends TestCase
         ($this->handler)($command);
 
         // Assert
-        $shoppingList = $this->shoppingListRepository->findOneById($shoppingList->getId());
+        $shoppingList = $this->shoppingListRepository->findOneById('shopping-list-id');
         $this->assertEmpty($shoppingList->getIngredients());
         $this->assertEmpty($shoppingList->getRows());
     }
@@ -68,33 +75,44 @@ class RemoveIngredientToShoppingListHandlerTest extends TestCase
     public function testHandleSuccessfullyRemoveIngredientToShoppingListHandlerReduceQuantity(): void
     {
         // Arrange
-        $user = new UserBuilder()->build();
-        $this->userRepository->save($user);
-        $ingredient = new IngredientBuilder()->isPasta()->build();
-        $this->ingredientRepository->save($ingredient);
         $shoppingList = new ShoppingListBuilder()
-            ->withOwner($user)
-            ->addIngredient($ingredient)
-            ->addIngredient($ingredient)
-            ->addIngredient($ingredient)
+            ->withId('shopping-list-id')
+            ->withOwner($this->user)
+            ->addIngredient($this->ingredient)
+            ->addIngredient($this->ingredient)
+            ->addIngredient($this->ingredient)
             ->build();
         $this->shoppingListRepository->save($shoppingList);
 
         $command = new RemoveIngredientToShoppingListCommand(
-            $user->getId(),
-            $ingredient->getSlug(),
+            'user-id',
+            'pate',
         );
-
 
         // Act
         ($this->handler)($command);
 
         // Assert
-        $shoppingList = $this->shoppingListRepository->findOneById($shoppingList->getId());
+        $shoppingList = $this->shoppingListRepository->findOneById('shopping-list-id');
         $this->assertCount(1, $shoppingList->getIngredients());
         $this->assertCount(1, $shoppingList->getRows());
-        $this->assertEquals(2, $shoppingList->getIngredients()->find(fn(ShoppingListIngredient $spi) => $spi->getIngredient()->isEqual($ingredient))->getQuantity());
-        $this->assertEquals(2, $shoppingList->getRows()->find(fn(ShoppingListRow $spr) => $spr->getIngredient()->isEqual($ingredient))->getQuantity());
+        $this->assertEquals(2, $shoppingList->getIngredients()->find(fn(ShoppingListIngredient $spi) => $spi->getIngredient()->isEqual($this->ingredient))->getQuantity());
+        $this->assertEquals(2, $shoppingList->getRows()->find(fn(ShoppingListRow $spr) => $spr->getIngredient()->isEqual($this->ingredient))->getQuantity());
+    }
+
+    public function testFailedHandleRemoveIngredientToShoppingListShoppingListNotFound(): void
+    {
+        // Arrange
+        $command = new RemoveIngredientToShoppingListCommand(
+            'user-id',
+            'pate',
+        );
+
+        // Assert
+        $this->expectExceptionObject(ShoppingListException::activeShoppingListNotFound());
+
+        // Act
+        ($this->handler)($command);
     }
 
 }
